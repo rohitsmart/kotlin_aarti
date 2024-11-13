344d0f56a7296cee409f79cd4bcc1752be4f393d
rohitsmart <rohitsmart95@gmail.com>
2024-11-13 18:30:27 +0530

    upadted

--- app/src/main/java/com/rohit/aarti/ui/theme/Theme.kt
+++ app/src/main/java/com/rohit/aarti/ui/theme/Theme.kt
@@ -1,46 +1,51 @@
 package com.rohit.aarti.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-// Define colors with updated names to avoid conflicts
-val Purple80Light = Color(0xFFD0BCFF)
-val PurpleGrey80Light = Color(0xFFCCC2DC) // Renamed for light theme
-val Pink80Light = Color(0xFFEFB8C8) // Renamed for light theme
-
-val Purple40Dark = Color(0xFF6650a4) // Renamed for dark theme
-val PurpleGrey40Dark = Color(0xFF625b71) // Renamed for dark theme
-val Pink40Dark = Color(0xFF7D5260) // Renamed for dark theme
-
-// Define Dark and Light Color Schemes
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple40Dark,
-    secondary = PurpleGrey40Dark,
-    tertiary = Pink40Dark
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple80Light,
-    secondary = PurpleGrey80Light,
-    tertiary = Pink80Light
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
 )
 
 @Composable
 fun MyApplicationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    // Choose the color scheme based on the system's dark theme setting
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
+
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
